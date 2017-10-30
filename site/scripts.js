23fdb761299571cab0c6eb95a223cd68c3123f29
Rodrigo Furlaneti <rodrigofurlaneti31@hotmail.com>
2017-10-30 12:30:34 -0200

    Add files via upload

---
+++ site/scripts.js
@@ -0,0 +1,10 @@
+function init()
+		{
+			var nome = document.getElementById("nome");
+			console.log(nome.value);
+			var telefone = document.getElementById("telefone");
+			console.log(telefone.value);
+			lista = new Array ();
+			lista.push(nome);
+			console.log(lista[0].value)
+		}	
